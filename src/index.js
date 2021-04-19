fd1cf084cfc26b38bd8bc1342bd9280c99d9e0c7
YAKSH BARIYA <yakshbari4@gmail.com>
2021-04-19 12:17:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,12 +25,31 @@ try {
  */
 const bot = new Discord.Client();
 
+
+
+async function updateLeaderboard() {
+	let leaderBoard = await Mongo.db()
+		.collection('main')
+		.find({}, { sort: [['count', -1]] })
+		.toArray()
+	let message = new Discord.MessageEmbed()
+	leaderBoard.forEach((member, index) => {
+		message.addField(`#${index + 1}`, `<@${member._id}>: ${member.count}`)
+	})
+	message.setTitle("Server leaderboard")
+	message.setThumbnail(msg.guild!.iconURL())
+	let guild = await bot.guilds.fetch('773131732043300865')
+ 	let channel = await guild.channels.fetch('832925876923400223')
+	channel.send(message)
+}
+
+
 /**
  * This function is called whenever the bot gets a message from any of the server in which it exists
  */
 bot.on("message", async function (message) {
 	// The message did not came from a bot user. So simply ignore it
-	if (!message.author.bot) return;
+	if (!message.author.bot) return
 	console.log(message.content.toString());
  	
 	await Mongo.db()
@@ -53,3 +72,5 @@ bot.on("message", async function (message) {
 
 // Log in out bot so that it can do it's work
 bot.login(process.env.BOT_TOKEN);
+
+setInterval(updateLeaderboard, 60000)
