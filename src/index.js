49bd2e4f89c5c8bd37923864b642869b3988aa04
YAKSH BARIYA <yakshbari4@gmail.com>
2021-04-19 12:01:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ bot.on("message", async function (message) {
 			}
 		)
 	let guild = await bot.guilds.fetch('773131732043300865')
- 	let channel = guild.channels.fetch('832925876923400223')
+ 	let channel = await guild.channels.fetch('832925876923400223')
 	channel.send(`${message.author}:   ${search.count+1}`)
 });
 
